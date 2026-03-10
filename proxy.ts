dafdc2d339a8ae33ace14dba7c0162e6957aa4c2
shakthi <shakthi2704@gmail.com>
2026-03-10 14:56:36 +0530

    fix: remove runtime config from proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/lib/auth"
 import { NextResponse } from "next/server"
 
-export const runtime = "nodejs"
+//export const runtime = "nodejs"
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth
